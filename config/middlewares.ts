c27bb10468f89d956352500df7888bd17a6d9550
syauri04 <syauri04@gmail.com>
2026-02-23 11:27:21 +0700

    update spaces object DO

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,12 +1,24 @@
 export default [
-  'strapi::logger',
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+  "strapi::logger",
+  "strapi::errors",
+  "strapi::security",
+  "strapi::cors",
+  "strapi::poweredBy",
+  "strapi::query",
+
+  {
+    name: "strapi::body",
+    config: {
+      formLimit: "256mb",
+      jsonLimit: "256mb",
+      textLimit: "256mb",
+      formidable: {
+        maxFileSize: 250 * 1024 * 1024, // 250MB
+      },
+    },
+  },
+
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
 ];
--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,24 @@
-export default () => ({});
+export default ({ env }) => ({
+  upload: {
+    config: {
+      provider: "aws-s3",
+      providerOptions: {
+        accessKeyId: env("DO_SPACE_KEY"),
+        secretAccessKey: env("DO_SPACE_SECRET"),
+        endpoint: env("DO_SPACE_ENDPOINT"),
+        region: env("DO_SPACE_REGION"),
+        params: {
+          Bucket: env("DO_SPACE_BUCKET"),
+        },
+        s3Options: {
+          forcePathStyle: false,
+        },
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
+      },
+    },
+  },
+});
