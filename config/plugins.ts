1960b7716c0f584771408af97363e84a7e1ce4e7
syauri04 <syauri04@gmail.com>
2026-02-23 14:14:21 +0700

    update config plugin

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,32 +1,26 @@
-export default ({ env }) => {
-  console.log("SPACE KEY:", env("DO_SPACE_KEY"));
-  console.log("SPACE SECRET:", env("DO_SPACE_SECRET") ? "ADA" : "KOSONG");
-  console.log("SPACE ENDPOINT:", env("DO_SPACE_ENDPOINT"));
-  console.log("SPACE REGION:", env("DO_SPACE_REGION"));
-  console.log("SPACE BUCKET:", env("DO_SPACE_BUCKET"));
-
-  return {
-    upload: {
-      config: {
-        provider: "aws-s3",
-        providerOptions: {
-          accessKeyId: env("DO_SPACE_KEY"),
-          secretAccessKey: env("DO_SPACE_SECRET"),
-          endpoint: env("DO_SPACE_ENDPOINT"),
+export default ({ env }) => ({
+  upload: {
+    config: {
+      provider: "aws-s3",
+      providerOptions: {
+        s3Options: {
+          credentials: {
+            accessKeyId: env("DO_SPACE_KEY"),
+            secretAccessKey: env("DO_SPACE_SECRET"),
+          },
           region: env("DO_SPACE_REGION"),
-          params: {
-            Bucket: env("DO_SPACE_BUCKET"),
-          },
-          s3Options: {
-            forcePathStyle: false,
-          },
+          endpoint: env("DO_SPACE_ENDPOINT"),
+          forcePathStyle: false,
         },
-        actionOptions: {
-          upload: {},
-          uploadStream: {},
-          delete: {},
+        params: {
+          Bucket: env("DO_SPACE_BUCKET"),
         },
       },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
+      },
     },
-  };
-};
+  },
+});
