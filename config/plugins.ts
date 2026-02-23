379b75d6efa6aa71df895b4a39bfa553f25afce8
syauri04 <syauri04@gmail.com>
2026-02-23 14:04:45 +0700

    console space env

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,5 +1,9 @@
 export default ({ env }) => {
   console.log("SPACE KEY:", env("DO_SPACE_KEY"));
+  console.log("SPACE SECRET:", env("DO_SPACE_SECRET") ? "ADA" : "KOSONG");
+  console.log("SPACE ENDPOINT:", env("DO_SPACE_ENDPOINT"));
+  console.log("SPACE REGION:", env("DO_SPACE_REGION"));
+  console.log("SPACE BUCKET:", env("DO_SPACE_BUCKET"));
 
   return {
     upload: {
