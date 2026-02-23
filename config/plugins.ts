5a2c2475ff8db070d8706cfa6901c557b2b0c00e
syauri04 <syauri04@gmail.com>
2026-02-23 12:04:57 +0700

    debug env provider

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,24 +1,28 @@
-export default ({ env }) => ({
-  upload: {
-    config: {
-      provider: "aws-s3",
-      providerOptions: {
-        accessKeyId: env("DO_SPACE_KEY"),
-        secretAccessKey: env("DO_SPACE_SECRET"),
-        endpoint: env("DO_SPACE_ENDPOINT"),
-        region: env("DO_SPACE_REGION"),
-        params: {
-          Bucket: env("DO_SPACE_BUCKET"),
+export default ({ env }) => {
+  console.log("SPACE KEY:", env("DO_SPACE_KEY"));
+
+  return {
+    upload: {
+      config: {
+        provider: "aws-s3",
+        providerOptions: {
+          accessKeyId: env("DO_SPACE_KEY"),
+          secretAccessKey: env("DO_SPACE_SECRET"),
+          endpoint: env("DO_SPACE_ENDPOINT"),
+          region: env("DO_SPACE_REGION"),
+          params: {
+            Bucket: env("DO_SPACE_BUCKET"),
+          },
+          s3Options: {
+            forcePathStyle: false,
+          },
         },
-        s3Options: {
-          forcePathStyle: false,
+        actionOptions: {
+          upload: {},
+          uploadStream: {},
+          delete: {},
         },
-      },
-      actionOptions: {
-        upload: {},
-        uploadStream: {},
-        delete: {},
       },
     },
-  },
-});
+  };
+};
